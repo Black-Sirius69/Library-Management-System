6e96db2721312dabe41e6ced47674233c0a33b51
Vedant-Matanhelia <vedant.matanhelia@gmail.com>
2022-01-20 15:33:11 +0530

    Added the login and logout functions

--- home.py
+++ home.py
@@ -1,24 +1,42 @@
-from PySide6 import QtWidgets
-from PySide6.QtWidgets import QMainWindow, QApplication
-from PySide6.QtUiTools import QUiLoader
+from PyQt6.QtWidgets import QLineEdit, QApplication, QMainWindow
+from PyQt6.uic.load_ui import loadUiType
 import sys
 
-class Library(QMainWindow):
+login, _ = loadUiType('./Library.ui')
+main, _ = loadUiType("./main.ui")
+
+class Library(QMainWindow, login):
     def __init__(self) -> None:
         super().__init__()
-        loader = QUiLoader();
-        self.ui = loader.load('./Library.ui', None)
-        self.ui.passwd.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.ui.login_btn.clicked.connect(self.login)
-        self.ui.show()
-    
+        self.setupUi(self)
+        self.passwd.setEchoMode(QLineEdit.EchoMode.Password)
+        self.login_btn.clicked.connect(self.login)
+
     def login(self):
-        if self.ui.user.text() == "admin" and self.ui.passwd.text() == 'admin':
-            print("Login Successfull.")
+        if self.user.text() == 'admin' and self.passwd.text() == 'admin':
+            self.main_win = MainWindow()
+            self.close()
+            self.main_win.show()
         else:
-            print("Login UnSuccessful.")
+            print("Not ")
+
+class MainWindow(QMainWindow, main):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setupUi(self)
+        self.logout_btn.clicked.connect(self.logout)
+
+    def logout(self):
+        self.login = Library()
+        self.close()
+        self.login.show()
+
+    def add_book(self):
+        "Open the add book ui"
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = Library()
+    win.show()
     sys.exit(app.exec())
