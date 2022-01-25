77f817e0ec4038118e235f3117b9b017344b557e
Vedant-Matanhelia <vedant.matanhelia@gmail.com>
2022-01-25 09:35:39 +0530

    Added the login and logout functions

--- home.py
+++ home.py
@@ -1,11 +1,11 @@
-from PyQt6.QtWidgets import QLineEdit, QApplication, QMainWindow
+from PyQt6.QtWidgets import QLineEdit, QApplication, QMainWindow, QMessageBox
 from PyQt6.uic.load_ui import loadUiType
 import sys
 
-login, _ = loadUiType('./Library.ui')
+login, _ = loadUiType('./Login.ui')
 main, _ = loadUiType("./main.ui")
 
-class Library(QMainWindow, login):
+class Login(QMainWindow, login):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
@@ -18,7 +18,10 @@ class Library(QMainWindow, login):
             self.close()
             self.main_win.show()
         else:
-            print("Not ")
+            error = QMessageBox()
+            error.about(self, "Could not login", "Incorrect username or password.")
+            error.setIcon(QMessageBox.Icon.Information)
+            error.show()
 
 class MainWindow(QMainWindow, main):
     def __init__(self) -> None:
@@ -27,16 +30,16 @@ class MainWindow(QMainWindow, main):
         self.logout_btn.clicked.connect(self.logout)
 
     def logout(self):
-        self.login = Library()
+        self.login = Login()
         self.close()
         self.login.show()
 
-    def add_book(self):
+    def add_books(self):
         "Open the add book ui"
         pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = Library()
+    win = Login()
     win.show()
     sys.exit(app.exec())
