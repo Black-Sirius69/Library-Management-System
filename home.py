0946ad2e894ad0510306e9683a07838ff9b10683
Vedant-Matanhelia <71623796+Vedant-Matanhelia@users.noreply.github.com>
2022-02-15 10:51:26 +0530

    Added images and the ability to import books from marc files

--- home.py
+++ home.py
@@ -1,16 +1,34 @@
+import os
 import sys
 
+import MySQLdb
 import sqlalchemy
-from sqlalchemy.exc import SQLAlchemyError
-from PySide6.QtWidgets import QLineEdit, QApplication, QMainWindow, QMessageBox
+from PySide6.QtWidgets import QLineEdit, QApplication, QMainWindow, QMessageBox, QFileDialog
+from pymarc import MARCReader
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
 import books
 import login
 import main_win
 
-# set the sqlalchemy engine to mysql server and connect
-engine = sqlalchemy.create_engine("mysql://vedant:vedant@localhost/library_management")
-conn = engine.connect()
+# import time
+
+Base = automap_base()
+
+#  set the sqlalchemy engine to mysql server and connect
+engine = sqlalchemy.create_engine(
+    "mysql://vedant:vedant@localhost/library_management")
+
+# Reflect tables
+Base.prepare(engine, reflect=True)
+
+Book = Base.classes.books
+Student = Base.classes.students
+
+# Create a session
+session = Session(engine)
 
 
 class Login(QMainWindow, login.Ui_MainWindow):
@@ -20,15 +38,17 @@ class Login(QMainWindow, login.Ui_MainWindow):
         self.setupUi(self)
         self.passwd.setEchoMode(QLineEdit.EchoMode.Password)
         self.login_btn.clicked.connect(self.login)
+        self.setWindowTitle("Cerebrum")
 
     def login(self):
-        if self.user.text() == 'admin' and self.passwd.text() == 'admin':
+        if self.user.text() == "admin" and self.passwd.text() == "admin":
             self.main_win = MainWindow()
             self.close()
             self.main_win.show()
         else:
             error = QMessageBox()
-            error.about(self, "Could not login", "Incorrect username or password.")
+            error.about(self, "Could not login",
+                        "Incorrect username or password.")
             error.setIcon(QMessageBox.Information)
             error.show()
 
@@ -37,7 +57,7 @@ class MainWindow(QMainWindow, main_win.Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.login = Login()
-        self.book = Books()
+        self.book = AddBooks()
         self.setupUi(self)
         self.logout_btn.clicked.connect(self.logout)
         self.add_book.clicked.connect(self.add_books)
@@ -51,12 +71,13 @@ class MainWindow(QMainWindow, main_win.Ui_MainWindow):
         self.book.show()
 
 
-class Books(QMainWindow, books.Ui_MainWindow):
+class AddBooks(QMainWindow, books.Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
         self.title.setFocus()
         self.add_data.clicked.connect(self.add)
+        self.import_data.clicked.connect(self.import_book)
 
     def add(self):
         try:
@@ -72,48 +93,102 @@ class Books(QMainWindow, books.Ui_MainWindow):
             call_num = self.call_number.text()
             edition = self.edition.text()
             copies = self.copies.text()
-            conn.execute(sqlalchemy.text("INSERT INTO books Values(:x, :a, :y, :z, :c, :e, :g, :f, :b, :d, :h, :i)"), {
-                "x": acc,
-                "y": author_1,
-                'z': author_2,
-                "a": title,
-                "b": class_num,
-                "c": pub,
-                "d": subject,
-                "e": isbn,
-                "f": call_num,
-                "g": yop,
-                "h": edition,
-                "i": copies})
+            book = Book(Title=title, Author_1=author_1, Author_2=author_2, isbn=isbn, call_no=call_num,
+                        class_no=class_num, copies=copies, publisher=pub, year_of_publication=yop,
+                        edition=edition, subject=subject, Acc_no=acc)
+            session.add(book)
+            session.commit()
             success = QMessageBox()
             success.about(self, "SUCCESSFUL", "Data Added")
-            success.setIcon(QMessageBox.Information)
             success.setStandardButtons(QMessageBox.StandardButton.Ok)
             success = success.exec()
             if success == QMessageBox.StandardButton.Ok:
                 for widget in self.findChildren(QLineEdit):
                     widget.clear()
         except SQLAlchemyError:
-            row = conn.execute(sqlalchemy.text("SELECT * FROM books WHERE Acc_no = :x"), {"x": self.acc_no.text()})
-            if len(list(row)) >= 1:
-                error = QMessageBox()
-                error.about(self, "Error", f"A Book with accession number {self.acc_no.text()} already exists")
-                error.setIcon(QMessageBox.Critical)
-                error = error.exec()
-                if error == QMessageBox.StandardButton.Ok:
-                    for widget in self.findChildren(QLineEdit):
-                        widget.clear()
-            else:
-                error = QMessageBox()
-                error.about(self, "Error", "Could not add data")
-                error.setIcon(QMessageBox.Critical)
-                error = error.exec()
-                if error == QMessageBox.StandardButton.Ok:
-                    for widget in self.findChildren(QLineEdit):
-                        widget.clear()
+            error = QMessageBox()
+            error.about(self, "Error", "Could not add data")
+            error.setIcon(QMessageBox.Critical)
+            error = error.exec()
+            if error == QMessageBox.StandardButton.Ok:
+                for widget in self.findChildren(QLineEdit):
+                    widget.clear()
+
+    def import_book(self):
+        skipped = []
+        name, _ = QFileDialog.getOpenFileName(self, "Select Marc File", os.getcwd(
+        ), "Marc Files (*.mrc);; CSV Files (*.csv);; All Files (*)")
+        if name != 0 and name[-4::] == '.mrc':
+            with open(name, 'rb') as file:
+                reader = MARCReader(file)
+                added = 0
+                for record in reader:
+                    title = record.title()
+                    author_1 = record.author()
+                    author_2 = None
+                    if record['541']:
+                        acc_no = record['541']['a']
+                        if type(acc_no) == str:
+                            pos = acc_no.find('J')
+                            acc_no = acc_no[0:pos]
+                    elif record['952']:
+                        acc_no = record['952']['p']
+                        if type(acc_no) == str:
+                            pos = acc_no.find('J')
+                            acc_no = acc_no[0:pos]
+                    else:
+                        print("Cannot Enter Data. Accession Number not found.")
+                        raise ValueError("Cannot Enter Data without Acc No.")
+
+                    if acc_no is None:
+                        skipped.append(record.title())
+                        continue
+
+                    if record['700']:
+                        author_2 = record['700']['a']
+                    publisher = record.publisher()
+                    yop = record.pubyear()
+                    if type(yop) == str:
+                        yop = None
+                    sub = None
+                    if record.subjects():
+                        sub = record.subjects()[0]['a']
+                    isbn = record.isbn()
+                    class_no = None
+                    call_no = None
+                    if record['082']:
+                        class_no = record['082']['a']
+                        call_no = record['082']['b']
+                    if type(class_no) == str:
+                        class_no = None
+                    edition = None
+                    if record['250']:
+                        edition = record['250']['a']
+                    copies = 1
+                    if record['562']:
+                        copies = record['562']['e']
+                    try:
+                        book = Book(Acc_no=acc_no, Title=title, Author_1=author_1, Author_2=author_2,
+                                    publisher=publisher, isbn=isbn, year_of_publication=yop, class_no=class_no,
+                                    call_no=call_no, subject=sub, edition=edition, copies=copies)
+                        session.add(book)
+                        session.commit()
+                        added += 1
+                    except MySQLdb.IntegrityError:
+                        skipped.append(title)
+                        session.rollback()
+                    except IntegrityError:
+                        skipped.append(title)
+                        session.rollback()
+                success = QMessageBox()
+                success.about(self, "Successful",
+                              f'Added {added} books.\n Could not add {len(skipped)} books as they were not compatible.')
 
 
-if __name__ == '__main__':
+session.close()
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = Login()
     win.show()
--- login.py
+++ login.py
@@ -15,52 +15,43 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QLineEdit,
-    QMainWindow, QPushButton, QSizePolicy, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QPushButton, QSizePolicy, QWidget)
+import library_management_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 596)
+        MainWindow.resize(474, 294)
+        MainWindow.setMaximumSize(QSize(474, 294))
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.frame = QFrame(self.centralwidget)
-        self.frame.setObjectName(u"frame")
-        self.frame.setMaximumSize(QSize(16777215, 50))
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.label = QLabel(self.frame)
+        self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(50, 10, 461, 31))
-
-        self.verticalLayout.addWidget(self.frame)
-
-        self.frame_2 = QFrame(self.centralwidget)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.user = QLineEdit(self.frame_2)
-        self.user.setObjectName(u"user")
-        self.user.setGeometry(QRect(260, 150, 113, 22))
-        self.passwd = QLineEdit(self.frame_2)
-        self.passwd.setObjectName(u"passwd")
-        self.passwd.setGeometry(QRect(260, 240, 113, 22))
-        self.login_btn = QPushButton(self.frame_2)
+        self.label.setGeometry(QRect(230, 60, 211, 41))
+        self.label.setStyleSheet(u"")
+        self.login_btn = QPushButton(self.centralwidget)
         self.login_btn.setObjectName(u"login_btn")
-        self.login_btn.setGeometry(QRect(280, 340, 111, 41))
-        self.Username = QLabel(self.frame_2)
+        self.login_btn.setGeometry(QRect(270, 230, 111, 31))
+        self.user = QLineEdit(self.centralwidget)
+        self.user.setObjectName(u"user")
+        self.user.setGeometry(QRect(340, 130, 113, 22))
+        self.Username = QLabel(self.centralwidget)
         self.Username.setObjectName(u"Username")
-        self.Username.setGeometry(QRect(146, 150, 91, 20))
-        self.Password = QLabel(self.frame_2)
+        self.Username.setGeometry(QRect(220, 130, 91, 20))
+        self.passwd = QLineEdit(self.centralwidget)
+        self.passwd.setObjectName(u"passwd")
+        self.passwd.setGeometry(QRect(340, 180, 113, 22))
+        self.Password = QLabel(self.centralwidget)
         self.Password.setObjectName(u"Password")
-        self.Password.setGeometry(QRect(150, 240, 91, 20))
-
-        self.verticalLayout.addWidget(self.frame_2)
-
+        self.Password.setGeometry(QRect(220, 180, 91, 20))
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(20, 60, 161, 151))
+        self.label_2.setStyleSheet(u"")
+        self.label_2.setPixmap(QPixmap(u":/images/logo.png"))
+        self.label_2.setScaledContents(True)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -74,5 +65,6 @@ class Ui_MainWindow(object):
         self.login_btn.setText(QCoreApplication.translate("MainWindow", u"Login", None))
         self.Username.setText(QCoreApplication.translate("MainWindow", u"Username", None))
         self.Password.setText(QCoreApplication.translate("MainWindow", u"Password", None))
+        self.label_2.setText("")
     # retranslateUi
 
