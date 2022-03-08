2fcf92383fd21c648740b957e553541af1cfabda
Vedant-Matanhelia <vedant.matanhelia@gmail.com>
2022-03-08 13:28:51 +0530

    Started the return books model and integrated the issue book one

--- issue_ui.py
+++ issue_ui.py
@@ -28,10 +28,16 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label_3 = QLabel(self.centralwidget)
-        self.label_3.setObjectName(u"label_3")
+        self.book_no = QLineEdit(self.centralwidget)
+        self.book_no.setObjectName(u"book_no")
 
-        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.book_no, 3, 1, 1, 1)
+
+        self.issue_date = QDateEdit(self.centralwidget)
+        self.issue_date.setObjectName(u"issue_date")
+        self.issue_date.setCalendarPopup(True)
+
+        self.gridLayout.addWidget(self.issue_date, 4, 1, 1, 1)
 
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
@@ -47,49 +53,48 @@ class Ui_MainWindow(object):
         self.label.setFont(font)
         self.label.setAlignment(Qt.AlignCenter)
 
-        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.label, 1, 1, 1, 1)
 
-        self.label_5 = QLabel(self.centralwidget)
-        self.label_5.setObjectName(u"label_5")
+        self.issue_book = QPushButton(self.centralwidget)
+        self.issue_book.setObjectName(u"issue_book")
 
-        self.gridLayout.addWidget(self.label_5, 4, 0, 1, 1)
-
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
-
-        self.issue_date = QDateEdit(self.centralwidget)
-        self.issue_date.setObjectName(u"issue_date")
-        self.issue_date.setCalendarPopup(True)
-
-        self.gridLayout.addWidget(self.issue_date, 3, 1, 1, 1)
+        self.gridLayout.addWidget(self.issue_book, 6, 1, 1, 1)
 
         self.ret_date = QDateEdit(self.centralwidget)
         self.ret_date.setObjectName(u"ret_date")
         self.ret_date.setCalendarPopup(True)
 
-        self.gridLayout.addWidget(self.ret_date, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.ret_date, 5, 1, 1, 1)
+
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 2, 0, 1, 1)
 
         self.stu_no = QLineEdit(self.centralwidget)
         self.stu_no.setObjectName(u"stu_no")
 
-        self.gridLayout.addWidget(self.stu_no, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.stu_no, 2, 1, 1, 1)
+
+        self.label_5 = QLabel(self.centralwidget)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout.addWidget(self.label_5, 5, 0, 1, 1)
 
         self.label_4 = QLabel(self.centralwidget)
         self.label_4.setObjectName(u"label_4")
 
-        self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_4, 4, 0, 1, 1)
 
-        self.book_no = QLineEdit(self.centralwidget)
-        self.book_no.setObjectName(u"book_no")
+        self.home_page = QPushButton(self.centralwidget)
+        self.home_page.setObjectName(u"home_page")
 
-        self.gridLayout.addWidget(self.book_no, 2, 1, 1, 1)
-
-        self.issue_book = QPushButton(self.centralwidget)
-        self.issue_book.setObjectName(u"issue_book")
-
-        self.gridLayout.addWidget(self.issue_book, 5, 1, 1, 1)
+        self.gridLayout.addWidget(self.home_page, 0, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         QWidget.setTabOrder(self.stu_no, self.book_no)
@@ -104,11 +109,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Book No.", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Issue A Book", None))
-        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Return Date", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Student No.", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Issue Date", None))
         self.issue_book.setText(QCoreApplication.translate("MainWindow", u"Issue book", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Book No.", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Student No.", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Return Date", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Issue Date", None))
+        self.home_page.setText(QCoreApplication.translate("MainWindow", u"Go Back", None))
     # retranslateUi
 
--- main_win.py
+++ main_win.py
@@ -5,19 +5,23 @@ import main_ui
 import books
 import login
 import students
+import issue_books
 
 class MainWindow(QMainWindow, main_ui.Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.login = login.Login()
         self.book = books.AddBooks()
+        self.issues = issue_books.Issue()
         self.student = students.Student()
         self.book.home_page.clicked.connect(lambda : self.go_home('book'))
         self.student.home_page.clicked.connect(lambda : self.go_home('student'))
+        self.issues.home_page.clicked.connect(lambda : self.go_home('issues'))
         self.setupUi(self)
         self.logout_btn.clicked.connect(self.logout)
         self.add_book.clicked.connect(self.add_books)
         self.add_student.clicked.connect(self.add_students)
+        self.issue_book.clicked.connect(self.issue)
         self.setWindowTitle("Cerebrum - Home")
 
     def logout(self):
@@ -32,11 +36,17 @@ class MainWindow(QMainWindow, main_ui.Ui_MainWindow):
         self.close()
         self.student.show()
 
+    def issue(self):
+        self.close()
+        self.issues.show()
+
     def go_home(self, btn_name):
         if btn_name == 'book':
             self.book.close()
         elif btn_name == 'student':
             self.student.close()
+        elif btn_name == 'issues':
+            self.issues.close()
         self.show()
 
 if __name__ == "__main__":
